d5c8e8bc9b55efa301353010cdf66c44f7929183
43517 <tingjue.du@wuxcl.com>
2019-04-08 18:18:48 +0800

    5

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        List<String> a=new ArrayList<>();
+        a.add("a");
+        a.add("b");
+        a.add("c");
+        List<String> b=null;
+        System.out.println(b.toString());
     }
 }
